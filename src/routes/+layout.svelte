17b274c336b2e2ec7e4d9b2408da03eb5674e82b
Talha5252 <90902659+Talha5252@users.noreply.github.com>
2025-11-24 10:18:17 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,7 +13,7 @@
 <div data-theme={theme} class=" min-h-screen p-2">
 	<select class="select absolute top-2 right-2 w-[150px]" bind:value={theme}>
 		<option disabled selected>Pick a theme</option>
-		<option value="light">light</option>
+		<option value="light">lightt</option>
 		<option value="dark">dark</option>
 		<option value="cyberpunk">cyberpunk</option>
 		<option value="valentine">valentine</option>
